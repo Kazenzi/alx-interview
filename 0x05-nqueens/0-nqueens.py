e92110a1732dc8d3f78bb5259dbde95246edfc85
Kazenzi <jacintakazenzi99@gmail.com>
2023-03-05 18:00:14 +0300

    mm

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,70 +1,73 @@
 #!/usr/bin/python3
 """
-Module for 0x0C. N Queens.
-Holberton School
-Specializations - Interview Preparation ― Algorithms
+Module 0-nqueens
+A program that solves the N queens problem
 """
-from sys import argv, exit
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+
+try:
+    num = int(sys.argv[1])
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
+
+if not (num >= 4):
+    print("N must be at least 4")
+    sys.exit(1)
 
 
 def solveNQueens(n):
-    """Program that solves the N queens problem"""
-    res = []
-    queens = [-1] * n
-    # queens is a one-dimension array, like [1, 3, 0, 2] means
-    # index represents row no and value represents col no
+    """Solution for n queens"""
+    col = set()  # keep track of used columns
+    pos = set()  # (r + c) keep track of used positive diagonals
+    neg = set()  # (r - c) keep track of used negative diagonals
 
-    def dfs(index):
-        """Recursively resolves the N queens problem"""
-        if index == len(queens):  # n queens have been placed correctly
-            res.append(queens[:])
-            return  # backtracking
-        for i in range(len(queens)):
-            queens[index] = i
-            if valid(index):  # pruning
-                dfs(index + 1)
+    res = []  # final result
 
-    # check whether nth queens can be placed
-    def valid(n):
-        """Method that checks if a position in the board is valid"""
-        for i in range(n):
-            if abs(queens[i] - queens[n]) == n - i:  # same diagonal
-                return False
-            if queens[i] == queens[n]:  # same column
-                return False
-        return True
+    board = [[] for n in range(n)]  # create empy board
 
-    # given queens = [1,3,0,2] this function returns
-    # [[0, 1], [1, 3], [2, 0], [3, 2]]
+    def backtrack(row):
+        """function for recursion"""
+        # means we've reached the last row
+        if row == n:
+            # get copy of current solution(current board)
+            copy = board.copy()
+            res.append(copy)
+            return
 
-    def make_all_boards(res):
-        """Method that builts the List that be returned"""
-        actual_boards = []
-        for queens in res:
-            board = []
-            for row, col in enumerate(queens):
-                board.append([row, col])
-            actual_boards.append(board)
-        return actual_boards
+        # for every column
+        for c in range(n):
+            # if we find that the column or diagonals are used, then skip
+            if c in col or (row + c) in pos or (row - c) in neg:
+                continue
 
-    dfs(0)
-    return make_all_boards(res)
+            # register found columns and diagonals
+            col.add(c)
+            pos.add(row + c)
+            neg.add(row - c)
+
+            board[row] = [row, c]
+
+            # move to next row
+            backtrack(row + 1)
+
+            # finally undo
+            col.remove(c)
+            pos.remove(row + c)
+            neg.remove(row - c)
+            board[row] = []
+
+    backtrack(0)
+
+    return res
 
 
 if __name__ == "__main__":
-    if len(argv) < 2:
-        print('Usage: nqueens N')
-        exit(1)
-    try:
-        n = int(argv[1])
-    except ValueError:
-        print('N must be a number')
-        exit(1)
+    boards = solveNQueens(num)
+    for board in boards:
 
-    if n < 4:
-        print('N must be at least 4')
-        exit(1)
-    else:
-        result = solveNQueens(n)
-        for row in result:
-            print(row)
+        print(board)
